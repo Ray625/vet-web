33715a6068a0f6165c8dda4d1e6dcfdc9ccc585e
Ray625 <wdvgy32@gmail.com>
2024-03-07 10:35:15 +0800

    feat: add homepage info section & style

---
+++ src/components/homepage_info.jsx
@@ -0,0 +1,54 @@
+import styles from '../styles/homepage_info..module.scss'
+
+const IconGroup = ({className, svg, content}) => {
+  return (
+    <div className={styles.iconGroup}>
+      <span className={styles.iconWrapper}>
+        <object data={`/svg/icon_` + svg +`.svg`} className={className} aria-label="icon"> </object>
+      </span>
+      <p>{content}</p>
+    </div>
+  )
+}
+
+const InfoSection = () => {
+  return (
+    <section className={styles.container}>
+      <div className={styles.wrapper}>
+        <div className={styles.infoSide}>
+          <div className={styles.titleGroup}>
+            <h2 className={styles.title}>
+              <object data="/svg/home_footprint.svg" className={styles.footprint} aria-label="footprint"> </object>
+              Infomation
+            </h2>
+            <h3 className={styles.subtitle}>本院資訊</h3>
+            <p className={styles.describe}>我們位於台北市立動物園旁，附近有數個停車場。若家中毛孩有任何異狀，請盡速與本院聯絡或線上預約。</p>
+          </div>
+          <div className={styles.infoGroup}>
+            <IconGroup 
+              className={styles.iconPhone}
+              svg={'phone'}
+              content={'02 2345 6789'}
+            />
+            <IconGroup 
+              className={styles.iconTime}
+              svg={'time'}
+              content={'Mon - Sun 10:00 am - 21:00 pm'}
+            />
+            <IconGroup 
+              className={styles.iconPin}
+              svg={'pin'}
+              content={'116台北市文山區新光路二段8號'}
+            />
+
+          </div>
+        </div>
+        <div className={styles.mapSide}>
+          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1590.2418086191021!2d121.57536631490653!3d24.996977295230206!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3442aa640ddea4d1%3A0xa4be333a46a96053!2z5YuV54mp5ZyS56uZ!5e0!3m2!1szh-TW!2stw!4v1709770702621!5m2!1szh-TW!2stw" className={styles.googleMap} title='map' allowfullscreen="true" loading="lazy" referrerpolicy="no-referrer-when-downgrade"> </iframe>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default InfoSection;
--- src/pages/homepage.jsx
+++ src/pages/homepage.jsx
@@ -6,6 +6,7 @@ import OfferSection from "../components/homepage_offer"
 import ReasonSection from "../components/homepage_why_choose_us"
 import ReviewsSection from "../components/homepage_reviews"
 import NewsSection from "../components/homepage_news"
+import InfoSection from "../components/homepage_info"
 import './homepage.scss'
 
 const Homepage = () => {
@@ -19,6 +20,7 @@ const Homepage = () => {
       <ReasonSection />
       <ReviewsSection />
       <NewsSection />
+      <InfoSection />
     </div>
   )
 }
